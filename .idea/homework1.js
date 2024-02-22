1e5d23ec996e027e097c1e6199a52aa7da5d700e
ViktoriaDzunusova <vikenti1996@gmail.com>
2024-02-22 15:40:03 +0200

    Initial commit

---
+++ .idea/homework1.js
@@ -0,0 +1,36 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+
+let  a='hello';
+    console.log(a);
+let m='owu';
+    console.log(m);
+let x='com'
+    console.log(x);
+let d='ua';
+    console.log(d);
+let e=1;
+    console.log(e);
+let f=10;
+console.log(f);
+let g=-999;
+console.log(g);
+let h=123;
+console.log(h);
+let i=true;
+console.log(i);
+let n=false;
+console.log(n)
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+let lastName='Dzunusova'
+let firstName='Viktoriia'
+let middleName='Olegovna'
+let sen=`${lastName} ${firstName} ${middleName}`
+console.log(sen);
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+    console.log(typeof 100);
+console.log(typeof '100');
+console.log(typeof true);
